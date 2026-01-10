dfe33aba05128c6fb2e7f2a299c6c6818abf5253
Dhanush Babu Ramadoss <dhanushramrt@gmail.com>
2026-01-09 18:03:26 -0800

    configured websocket

--- game-engine/src/main/java/com/realityGameShow/game/engine/ws/config/WebSocketConfig.java
+++ game-engine/src/main/java/com/realityGameShow/game/engine/ws/config/WebSocketConfig.java
@@ -1,19 +1,23 @@
 package com.realityGameShow.game.engine.ws.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-public class WebSocketConfig implements WebSocketConfigurer{
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
     private final WebSocketHandler gameWebSocketHandler;
 
-    public WebSocketConfig(WebSocketHandler gameWebSocketHandler){
+    public WebSocketConfig(WebSocketHandler gameWebSocketHandler) {
         this.gameWebSocketHandler = gameWebSocketHandler;
     }
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry){
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(gameWebSocketHandler, "/ws/game").setAllowedOrigins("*");
     }
-    
 }
